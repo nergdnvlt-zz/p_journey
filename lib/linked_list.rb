3d154a8df251d8ddecd7dce21d7ff22b41e5a0b4
nergdvnlt <tylerlundgren@yahoo.com>
2018-01-30 13:41:35 -0700

    Iteration 2 finished.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -3,20 +3,39 @@ require './lib/node.rb'
 
 class LinkedList
   attr_reader :head,
-              :count
+              :count,
+              :tail,
+              :arr_surnames,
+              :intp_array
 
   def initialize(head=nil)
     @head         = head
     @count        = 0
+    @tail         = nil
+    @arr_surnames = []
+    @intp_array   = []
   end
 
   def append(data)
     @count += 1
-    @head = Node.new(data)
+    if @head == nil
+      @head = Node.new(data)
+      @arr_surnames << @head.surname
+    else
+      @head.next_node = Node.new(data)
+      @tail = @head.next_node
+      @arr_surnames << @tail.surname
+    end
   end
 
   def to_string
-    "The #{@head.surname} family"
+    if @count == 1
+      "The #{@arr_surnames[0]} family"
+    else
+      @intp_array.push("The #{@arr_surnames[0]} family")
+      @intp_array.push(", followed by the #{@arr_surnames[1]} family")
+      @intp_array.join
+    end
   end
 
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -27,19 +27,11 @@ class Linked_List_Test < Minitest::Test
     assert_equal "West", list.head.surname
   end
 
-  def test_append_with_other_values
-    list = LinkedList.new
-
-    list.append("Erikkson")
-
-    assert_equal "Erikkson", list.head.surname
-  end
-
-  def test_list_being_built
+  def test_list_returns_correct_list
     list = LinkedList.new
     list.append("West")
 
-    assert_instance_of LinkedList, list
+    assert LinkedList, list
   end
 
   def test_result_of_heads_next_node
@@ -62,4 +54,76 @@ class Linked_List_Test < Minitest::Test
 
     assert_equal "The West family", list.to_string
   end
+
+  ########### New Iteration - New name and subsequent nodes
+
+  def test_different_data_as_head
+      list = LinkedList.new
+
+      list.append("Rhodes")
+
+      assert_instance_of Node, list.head
+      assert_equal "Rhodes", list.head.surname
+  end
+
+  def test_new_list_returns_correctly
+      list = LinkedList.new
+
+      list.append("Rhodes")
+
+      assert LinkedList, list
+  end
+
+  def test_correct_node_creation
+      list = LinkedList.new
+
+      list.append("Rhodes")
+      assert Node, list.head
+  end
+
+  def test_new_head_next_node
+      list = LinkedList.new
+
+      list.append("Rhodes")
+
+      assert_nil list.head.next_node
+  end
+
+  def test_append_additional_node
+      list = LinkedList.new
+
+      list.append("Rhodes")
+      list.append("Hardy")
+
+      assert_instance_of Node, list.head.next_node
+  end
+
+  def test_can_call_next_surname
+    list = LinkedList.new
+
+    list.append("Rhodes")
+    list.append("Hardy")
+
+    assert "Hardy", list.head.next_node.surname
+  end
+
+  def test_counter
+    list = LinkedList.new
+
+    list.append("Rhodes")
+    list.append("Hardy")
+
+    assert 2, list.count
+  end
+
+  def test__node_interpolation
+    list = LinkedList.new
+
+    list.append("Rhodes")
+    list.append("Hardy")
+
+    assert_equal "The Rhodes family, followed by the Hardy family", list.to_string
+  end
+
+
 end

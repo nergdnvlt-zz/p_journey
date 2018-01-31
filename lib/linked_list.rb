674324a1c36768f5d4d9e3d3c423f14d1ff248d4
nergdvnlt <tylerlundgren@yahoo.com>
2018-01-30 22:58:38 -0700

    Finally fixed nodes and count...

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,31 +7,47 @@ class LinkedList
 
 
   def initialize(head=nil)
-    @head         = head
-    @node_count   = 0
+    @head = head
   end
 
-  # def append(data)
-  #   if @head == nil
-  #     @head = Node.new(data)
-  #   else
-  #     @head.next_node = Node.new(data)
-  #   end
-  # end
 
   def append(data)
     node = Node.new(data)
-    current_node = @head
+
     if @head == nil
       @head = node
+
     else
-      @head.next_node = node
-      current_node = @head.next_node
-      # until current_node.next_node.nil?
-      #   current_node = Node.new(data)
-      #   current_node = current_node.next_node
-      # end
+      current_node = @head
+
+      until current_node.next_node.nil?
+        current_node = current_node.next_node
+      end
+
+      current_node.next_node = node
     end
+  end
+
+
+  def count
+    counter = 0
+
+    if @head.nil?
+      counter
+
+    elsif @head.next_node.nil?
+      counter += 1
+
+    else
+      current_node = @head
+
+      while current_node != nil
+        counter += 1
+        current_node = current_node.next_node
+      end
+
+    end
+    counter
   end
 
   # def to_string
@@ -47,22 +63,6 @@ class LinkedList
   #   end
   #   return familes
   #   # "The #{current.surname} family"
-  # end
-
-  # def count
-  #   current_node = @head.next_node
-  #
-  #   if @head.nil?
-  #     @node_count = 0
-  #   elsif @head.next_node.nil?
-  #     @node_count = 1
-  #   else @node_count = 1
-  #     until current_node.nil?
-  #       current_node = current_node.next_node
-  #       @node_count += 1
-  #     end
-  #   end
-  #   @node_count
   # end
 
 
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -62,7 +62,7 @@ class Linked_List_Test < Minitest::Test
   end
 
   ################# Testing Append - Multiple Nodes
-  
+
   def test_append_additional_node
       list = LinkedList.new
 
@@ -72,97 +72,111 @@ class Linked_List_Test < Minitest::Test
       assert_instance_of Node, list.head.next_node
   end
 
-  # def test_can_call_next_surname
-  #   list = LinkedList.new
-  #
-  #   list.append("Rhodes")
-  #   list.append("Hardy")
-  #
-  #   assert_equal "Hardy", list.head.next_node.surname
-  # end
-  #
-  # def test_many_appends
-  #   list = LinkedList.new
-  #
-  #   assert_nil list.head
-  #
-  #   list.append("Swift")
-  #   assert_instance_of Node, list.head
-  #   assert_equal "Swift", list.head.surname
-  #
-  #   list.append("West")
-  #   assert_instance_of Node, list.head.next_node
-  #   assert_equal "West", list.head.next_node.surname
-  #
-  #   list.append("Lovato")
-  #   list.append("Gomez")
-  #   list.append("Bieber")
-  #
-  #   assert_instance_of Node, list.head
-  #                                .next_node
-  #                                .next_node
-  #                                .next_node
-  #                                .next_node
-  #   assert_equal "Bieber", list.head
-  #                              .next_node
-  #                              .next_node
-  #                              .next_node
-  #                              .next_node
-  #                              .surname
-  #
-  # end
+  def test_can_call_next_surname
+    list = LinkedList.new
+
+    list.append("Rhodes")
+    list.append("Hardy")
+
+    assert_equal "Hardy", list.head.next_node.surname
+  end
+
+  def test_can_call_three_appends
+    list = LinkedList.new
+
+    list.append("Rhodes")
+    assert_equal "Rhodes", list.head.surname
+
+    list.append("Hardy")
+    assert_equal "Hardy", list.head.next_node.surname
+
+    list.append("Thor")
+    assert_equal "Thor", list.head.next_node.next_node.surname
+  end
+
+
+  def test_many_appends
+    list = LinkedList.new
+
+    assert_nil list.head
+
+    list.append("Swift")
+    assert_instance_of Node, list.head
+    assert_equal "Swift", list.head.surname
+
+    list.append("West")
+    assert_instance_of Node, list.head.next_node
+    assert_equal "West", list.head.next_node.surname
+
+    list.append("Lovato")
+    list.append("Gomez")
+    list.append("Bieber")
+
+    assert_instance_of Node, list.head
+                                 .next_node
+                                 .next_node
+                                 .next_node
+                                 .next_node
+    assert_equal "Bieber", list.head
+                               .next_node
+                               .next_node
+                               .next_node
+                               .next_node
+                               .surname
+
+  end
 
 
 ################# Testing Count Method
 
-  # def test_list_count_method
-  #   list = LinkedList.new
-  #   list.append("West")
-  #
-  #   assert_equal 1, list.count
-  #
-  # end
-  #
-  # def test_counter
-  #   list = LinkedList.new
-  #
-  #   list.append("Rhodes")
-  #   assert_equal 1, list.count
-  #
-  #   list.append("Hardy")
-  #   assert_equal 2, list.count
-  # end
-  #
-  # def test_count_for_three
-  #   list = LinkedList.new
-  #
-  #   list.append("Rhodes")
-  #   assert_equal 1, list.count
-  #
-  #   list.append("Hardy")
-  #   assert_equal 2, list.count
-  #
-  #   list.append("Odin")
-  #   assert_equal 3, list.count
-  # end
-  #
-  # def test_count
-  #
-  #   list = LinkedList.new
-  #
-  #   assert_equal 0, list.count
-  #
-  #   list.append("a")
-  #   assert_equal 1, list.count
-  #
-  #   list.append("b")
-  #   assert_equal 2, list.count
-  #
-  #   list.append("c")
-  #   list.append("d")
-  #   list.append("e")
-  #   assert_equal 5, list.count
-  # end
+  def test_list_count_method
+    list = LinkedList.new
+    list.append("West")
+
+    assert_equal 1, list.count
+
+  end
+
+  def test_counter
+    list = LinkedList.new
+
+    list.append("Rhodes")
+    assert_equal 1, list.count
+
+    list.append("Hardy")
+    assert_equal 2, list.count
+  end
+
+  def test_count_for_three
+    list = LinkedList.new
+
+    list.append("Rhodes")
+    assert_equal 1, list.count
+
+    list.append("Hardy")
+    assert_equal 2, list.count
+
+    list.append("Odin")
+    assert_equal 3, list.count
+  end
+
+  def test_count
+
+    list = LinkedList.new
+
+    assert_equal 0, list.count
+
+    list.append("a")
+    assert_equal 1, list.count
+
+    list.append("b")
+    assert_equal 2, list.count
+
+    list.append("c")
+    list.append("d")
+    list.append("e")
+    assert_equal 5, list.count
+  end
 
 ########### Test String Method
 
